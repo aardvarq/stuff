7b6e90fc2f7fef5abcc711cba0cc4adbc0708420
aardvarq <lunix.captain@gmail.com>
2022-04-29 14:56:25 -0600

    add some verbosity

--- util.sh
+++ util.sh
@@ -4,12 +4,14 @@ if [[ ! $2 ]]; then
 fi 
 
 git checkout master 1>/dev/null 2>&1
+
+echo "Fetching record list..."
 git pull origin  1>/dev/null 2>&1
 
 ./util.py $1 $2
 
 if [[ $1 == add || $1 == remove ]] ; then
-  echo "Updating source of record"
+  echo "Updating record list..."
   git add records.json 1>/dev/null 2>&1
   git commit -m "update records.json" 1>/dev/null 2>&1
   git push origin master 1>/dev/null 2>&1
